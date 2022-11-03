9018171d7c6e00ad843522489fd285dace39f5a3
gabol2 <90797014+gabol2@users.noreply.github.com>
2022-11-02 21:03:06 -0600

    Plantilla del Login y Copié las clases de geo para la base de datos

---
+++ src/main/java/Controller/beanIngreso.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import java.io.IOException;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Gabri
+ */
+public class beanIngreso {
+    String usuario = "";
+    String clave = "";
+
+    String mensaje = "";
+
+    public void Ingresar() throws IOException {    
+        
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
